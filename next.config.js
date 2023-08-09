f75d5e02f64197a40a46b00bcf8e5b489d02caad
suyongyi <1124144330@qq.com>
2023-08-09 14:32:21 +0800

    feat: blog v1.0.0

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  // 给文档中的代码块添加 copy 能力
+  defaultShowCopyCode: true,
+  // 支持 latex
+  latex: true,
+  // 支持静态图片
+  staticImage: true,
+});
+
+const nextConfig = {
+  // next 工程本身配置内容
+  reactStrictMode: true,
+};
+
+module.exports = withNextra(nextConfig);
